a5b13ce95e0d74b278fda03d80449de82f150a7c
Olawill <henry.williams658@gmail.com>
2024-10-04 17:25:10 -0600

    Edit waitlist modal

---
+++ app/pricing/page.tsx
@@ -0,0 +1,7 @@
+const PricingPage = () => {
+  return (
+    <div className="min-h-screen container mx-auto pt-[23px]">PricingPage</div>
+  );
+};
+
+export default PricingPage;
---
+++ app/why-properly/page.tsx
@@ -0,0 +1,9 @@
+const WhyProperlyPage = () => {
+  return (
+    <div className="min-h-screen container mx-auto pt-[23px]">
+      WhyProperlyPage
+    </div>
+  );
+};
+
+export default WhyProperlyPage;
